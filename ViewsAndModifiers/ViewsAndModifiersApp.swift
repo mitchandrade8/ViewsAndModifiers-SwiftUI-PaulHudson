50360eae2d55a67e18c6f7faad2cf7117f4f6970
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-10 19:24:12 -0700

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Mitch Andrade on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
